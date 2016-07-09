ec1ef51f3b45f25b15a1b8337b43cda8bc4e3581
hoang nguyen <hoang nguyen>
2016-07-09 23:06:43 +0700

    layout files - 5

---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+        <meta charset="utf-8">
+
+    </head>
+    <body>    
+
+        @yield('content')
+      
+    </body>
+</html>
