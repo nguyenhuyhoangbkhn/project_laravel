bb171fbad7d1d064fbf45b06e477567f3e0116a5
hoang nguyen <hoang nguyen>
2016-07-09 15:47:08 +0700

    push project in github

---
+++ app/Http/routes.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+	$people = ['Hoang','Huy','Nguyen'];
+    return view('welcome',compact('people'));
+});
+
+
+Route::get('about', function () {
+    return view('page.about'); //resoure/views/page/about.blade.php 
+});
